00bc77e00e725975ec3d4ffc7c3c54b5e2c1c8eb
Subho-n5 <43522742+Subho-n5@users.noreply.github.com>
2021-07-12 23:46:30 +0530

    Update sound_sensing.ino

--- sound_sensing_led/sound_sensing.ino
+++ sound_sensing_led/sound_sensing.ino
@@ -3,22 +3,18 @@ int val;
 float cal;
 
 void setup() {
-  // put your setup code here, to run once:
+  //Here we will use analogue out from pins 3, 5, 6, 9, 10, 11
 Serial.begin(9600);
 pinMode(A0, INPUT);
 pinMode(5, OUTPUT);
-pinMode(9, OUTPUT);
-/*
-while (Serial.available()==0){}//wait for user input
-int x = Serial.parseInt(); //for taking user input
-analog_led_write_val = 51*(desired value)
-range of the value is 0-255
-*/
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  val = analogRead(A0);
+//analog_led_write_val = 51*(desired value)
+//range of the value to be written is between 0-255
+
+void loop() 
+{
+val = analogRead(A0);
 
 if (val<970)
   cal = val/475;//1920
@@ -31,8 +27,5 @@ else if (val<1000)
 else
   cal = val/205;
 Serial.println(cal);
-analogWrite(5,cal);
-analogWrite(9,cal);
-//delay(100);
-
+analogWrite(5,(cal*51));
 }
